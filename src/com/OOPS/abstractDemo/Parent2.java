e36c0735e07bdd99de82cddc6797885e0c6c0b85
Kshawayushi28 <shawayushi110@gmail.com>
2025-09-13 16:09:29 +0530

    interfaces

---
+++ src/com/OOPS/abstractDemo/Parent2.java
@@ -0,0 +1,24 @@
+package com.OOPS.abstractDemo;
+
+public abstract class Parent2 {
+
+    int age;
+    final int VALUE;
+
+    public Parent2(int age){
+        this.age = age;
+        VALUE = 32456789;
+    }
+
+    static void hello(){
+        System.out.println("Hey");
+    }
+
+    void normal() {
+        System.out.println("this is a normal method");
+    }
+
+    abstract void career();
+    abstract void partner();
+
+}
---
+++ src/com/OOPS/interfaces/Media.java
@@ -0,0 +1,6 @@
+package com.OOPS.interfaces;
+
+public interface Media {
+    void start();
+    void stop();
+}
