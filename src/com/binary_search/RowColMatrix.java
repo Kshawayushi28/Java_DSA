ae16b2886f001365eb0af842c60f5289c7cfb20d
Kshawayushi28 <shawayushi110@gmail.com>
2025-11-02 01:31:12 +0530

    binary_search

---
+++ src/com/binary_search/RowColMatrix.java
@@ -0,0 +1,33 @@
+package com.binary_search;
+
+import java.util.Arrays;
+
+public class RowColMatrix {
+    public static void main(String[] args) {
+        int[][] arr = {
+                {10,20,30,40},
+                {15,25,35,45},
+                {28,29,37,49},
+                {33,34,38,50}
+        };
+
+        System.out.println(Arrays.toString(search(arr, 49)));
+    }
+
+    static int[] search(int[][] matrix, int target) {
+        int r = 0;
+        int c = matrix[0].length - 1; // last column index
+
+        while (r < matrix.length && c >= 0) {
+            if (matrix[r][c] == target) {
+                return new int[]{r, c};
+            }
+            if (matrix[r][c] > target) {
+                c--; // move left
+            } else {
+                r++; // move down
+            }
+        }
+        return new int[]{-1, -1};
+    }
+}
